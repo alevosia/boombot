ead134c7b76bd7d27c473147911204f81a9dc1b7
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2022-02-02 18:39:52 +0800

    Check if BOT_TOKEN is defined

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,14 @@ if (process.env.NODE_ENV !== 'production') {
     dotenv.config()
 }
 
+const BOT_TOKEN = process.env.BOT_TOKEN
+
+if (!BOT_TOKEN) {
+    throw new Error('Environment variable BOT_TOKEN is missing.')
+}
+
 const client = new SapphireClient({
     intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'],
 })
 
-client.login(process.env.BOT_TOKEN)
+client.login(BOT_TOKEN)
