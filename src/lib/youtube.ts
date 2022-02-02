b7457579f8e7b86d996bf48e5957416a44bae39d
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2022-02-03 02:36:54 +0800

    Remove YOUTUBE_API_KEY env variable

--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -1,15 +1,4 @@
-import * as dotenv from 'dotenv'
 import * as yt from 'youtube-search-without-api-key'
-
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config()
-}
-
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY
-
-if (!YOUTUBE_API_KEY) {
-    throw new Error('Environment variable YOUTUBE_API_KEY is missing.')
-}
 
 export async function searchVideoByTitle(title: string) {
     const results = await yt.search(title)
