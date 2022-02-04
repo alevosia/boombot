cc4aaedcfc5b9e3e1b24c4119045f796cb4da7fa
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2022-02-04 16:33:58 +0800

    Remove constructor of ErrorListener

--- src/listeners/error.ts
+++ src/listeners/error.ts
@@ -1,14 +1,6 @@
 import { Listener } from '@sapphire/framework'
 
 export class ErrorListener extends Listener {
-    public constructor(context: Listener.Context, options: Listener.Options) {
-        super(context, {
-            ...options,
-            once: true,
-            event: 'error',
-        })
-    }
-
     public run(error: Error) {
         this.container.logger.error(
             'Encountered an error!',
